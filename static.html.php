b25e730c897fe6dd1a5ff6fec9ac285077b8e48f
danpros <dan@danpros.com>
2021-02-26 20:02:31 +0700

    Update static.html.php

--- static.html.php
+++ static.html.php
@@ -1,7 +1,7 @@
 <article class="page post">
 		<header class="entry-header">
 		<?php if (login()) { echo '<div class="entry-title-meta"><span><a href="'. $p->url .'/edit?destination=post">Edit</a></span></div>'; } ?>	
-					<h1 class="entry-title p-name"><?php echo $p->title;?></h1>
+					<h1 class="entry-title p-name" style="margin-bottom:20px;"><?php echo $p->title;?></h1>
 			</header>
 	<!-- .entry-header -->
 
