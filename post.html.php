5b825c666fc73f79b5064f7f6d99f932545a3af1
danpros <dan@danpros.com>
2021-03-19 12:08:45 +0700

    Update post.html.php

--- post.html.php
+++ post.html.php
@@ -93,9 +93,6 @@
 <?php if (disqus()): ?>
     <?php echo disqus($p->title, $p->url) ?>
 <?php endif; ?>
-<?php if (disqus_count()): ?>
-    <?php echo disqus_count() ?>
-<?php endif; ?>
 <?php if (facebook() || disqus()): ?>
 <div class="comments-area" id="comments" style="margin:30px 0 0 0;">
     <?php if (facebook()): ?>
